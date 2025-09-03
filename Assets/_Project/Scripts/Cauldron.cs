f039f1e60ffde2ba17240db3198d7dd7c3cda2bb
LilianAragou <114906517+LilianAragou@users.noreply.github.com>
2025-09-03 17:22:53 +0200

    add cooking

--- Assets/_Project/Scripts/Cauldron.cs
+++ Assets/_Project/Scripts/Cauldron.cs
@@ -2,8 +2,33 @@ using UnityEngine;
 using System.Collections;
 public class Cauldron : MonoBehaviour
 {
-    private void OnMouseDown()
+    private bool isCooking = false;
+    private float cookTimer;
+    void Update()
     {
-        Debug.Log("click");
+        if (isCooking)
+        {
+            cookTimer += Time.deltaTime;
+        }
+        else
+        {
+            if (cookTimer > 0f)
+            {
+                checkRecipe(cookTimer);
+            }
+            cookTimer = 0f;
+        }
+    }
+    private void OnMouseDrag()
+    {
+        isCooking = true;
+    }
+    private void OnMouseUp()
+    {
+        isCooking = false;
+    }
+    private void checkRecipe(float cookTimer)
+    {
+        Debug.Log("Cooked for " + cookTimer + " seconds.");
     }
 }
